f793ba175328edc6195ef0670d1d58f22f7b6ef5
canifty <can-dindar@outlook.com>
2024-12-09 11:56:30 +0100

    Initial project setup

---
+++ pricePlanner/pricePlanner/pricePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pricePlannerApp.swift
+//  pricePlanner
+//
+//  Created by Can Dindar on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct pricePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
